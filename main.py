4e0cbac21bbf5c2cf3dd6b245182e67ea3162bd1
EasChen70 <eason_chen@uri.edu>
2024-08-26 18:42:12 -0400

    Consensus algorithm to resolve conflicts and establish authoritative chain

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 from urllib.parse import urlparse
+import requests
 
 class Blockchain():
     def __init__(self):
@@ -19,6 +20,47 @@ class Blockchain():
         #Add new node to list of nodes, takes address of node
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            #check if hash is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            #check if proof of work is correct
+            if not self.validate_proof(last_block['proof'], block['proof']):
+                return False
+            
+            last_block = block
+            current_index += 1
+        return True
+
+    def consensus_algo(self):
+        neighbors = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+            
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                #check length
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+            #replace if found
+            if new_chain:
+                self.chain = new_chain
+                return True
+            
+        return False
 
     def new_block(self, proof, previous_hash=None):
         #Block structure, timestamp, list of transactions, proof, hash of prev block
@@ -129,6 +171,36 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     #200 http status ok
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods = ['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error, invalid list of Nodes", 400
+    for node in nodes:
+        blockchain.register_node(node)
+    response = {
+        'message': "New nodes have been added",
+        'total_nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods = ['GET'])
+def consensus():
+    replaced = blockchain.consensus_algo()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
     return jsonify(response), 200
 
 if __name__ == '__main__':
